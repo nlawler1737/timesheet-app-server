30dc9934b30970eaf04b7b3c77a7842832bbdbc8
Nolan Lawler <nlawler0207@gmail.com>
2024-04-22 22:09:43 -0700

    added create account endpoint

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiService } from './api.service';
 
 @Controller('api')
@@ -8,5 +8,20 @@ export class ApiController {
   @Get('hello-world')
   helloWorld(): string {
     return this.apiService.helloWorld();
+  }
+
+  @Post('/auth/create')
+  createUser(
+    @Body()
+    createUserData: {
+      email: string;
+      password: string;
+      confirmPassword: string;
+    },
+  ) {
+    return this.apiService.createUser({
+      email: createUserData.email,
+      password: createUserData.password,
+    });
   }
 }
--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ApiService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
   helloWorld(): string {
     return 'Hello world';
+  }
+
+  createUser({ email, password }) {
+    const createdUser = this.userRepository.create({
+      email,
+      hashed_password: 'hashed_' + password,
+    });
+    this.userRepository.save(createdUser);
   }
 }
